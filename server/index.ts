979fb47f83b2f80087dfbbe5607c89f2aacc802e
olaghislain <136685572+olaghislain@users.noreply.github.com>
2025-07-30 12:49:13 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
  app.listen(PORT, '0.0.0.0', () => {
-  console.log(Server running on http://0.0.0.0:${PORT});
+  console.log('Server running on http://0.0.0.0:${PORT}');
 });
 
 })();
